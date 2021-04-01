a6a03a99bdf321e185bb2f59ce6cc27f0902696a
Ryan Ha <ryanha.mail@gmail.com>
2021-04-01 19:31:13 -0400

    Added looping functionality for reminders

--- notificationCog.py
+++ notificationCog.py
@@ -1,5 +1,6 @@
-from datetime import date
-from discord.ext import commands
+from datetime import datetime, timedelta
+from discord.ext import commands, tasks
+import asyncio
 
 
 class NotificationCog(commands.Cog):
@@ -9,27 +10,47 @@ class NotificationCog(commands.Cog):
         self.lastRunTime = None
         self.notificationText = None
         self.notificationLink = None
+        self.runInterval = 0
+        self.ctx = None
+        self.ctxLock = asyncio.Lock()
+        self.started = False
 
-    @commands.Cog.listener()
-    async def on_message(self, message):
-        if message.author == self.bot.user:
-            return
+    @tasks.loop(seconds=10.0)
+    async def check_reminder(self):
+        if (self.lastRunTime is None or self.lastRunTime + timedelta(seconds=self.runInterval) < datetime.now()) \
+                and self.notificationText is not None:
+            await self.send_reminder(self.ctx, self.notificationText)
 
-        if (self.lastRunTime is None or self.lastRunTime < date.today()) and self.notificationText is not None:
-            await self.send_reminder(message, self.notificationText)
+        await self.ctx.send('lastRunTime is: {0}'.format(self.lastRunTime))
 
-        await message.channel.send('lastRunTime is: {0}'.format(self.lastRunTime))
-
-    @commands.command()
-    async def link(self, ctx, arg):
-        self.notificationLink = arg
-        await ctx.send('Set: {0}'.format(arg))
+    @check_reminder.after_loop
+    async def on_check_remind_cancel(self):
+        print("done")
         return
 
     @commands.command()
-    async def text(self, ctx, arg):
+    async def link(self, _, arg):
+        self.notificationLink = arg
+        return
+
+    @commands.command()
+    async def text(self, _, arg):
         self.notificationText = arg
-        await ctx.send('Set: {0}'.format(arg))
+        return
+
+    @commands.command()
+    async def interval(self, _, arg):
+        self.runInterval = arg
+        return
+
+    @commands.command()
+    async def start(self, ctx):
+        async with self.ctxLock:
+            self.ctx = ctx
+
+        if self.started is False:
+            self.check_reminder.start()
+
         return
 
     @commands.command(name='set')
@@ -43,10 +64,14 @@ class NotificationCog(commands.Cog):
         await ctx.send('Set: {0} and {1}'.format(args[0], args[1]))
         return
 
-    async def send_reminder(self, message, notification):
+    @check_reminder.before_loop
+    async def before_check_reminder_loop(self):
+        await self.bot.wait_until_ready()
+
+    async def send_reminder(self, ctx, notification):
         if notification is not None:
-            await message.channel.send('{0}\n{1}'.format(notification,
-                                                         self.notificationLink if
-                                                         self.notificationLink is not None
-                                                         else ""))
-        self.lastRunTime = date.today()
+            await ctx.send('{0}\n{1}'.format(notification,
+                                             self.notificationLink if
+                                             self.notificationLink is not None
+                                             else ""))
+        self.lastRunTime = datetime.now()
