db13f2597f023a3ffaa6d7047061a9960c6fe4fd
Ryan Ha <ryanha.mail@gmail.com>
2021-04-01 23:32:19 -0400

    Add locking on properties that can be set

--- notificationCog.py
+++ notificationCog.py
@@ -8,35 +8,42 @@ check_reminder_interval = 1.0
 class NotificationCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self._last_member = None
         self.lastRunTime = None
-        self.notificationText = None
-        self.notificationLink = None
-        self.runInterval = 0
         self.ctx = None
+        self.notification_text = None
+        self.notification_link = None
+        self.runInterval = 0
         self.ctxLock = asyncio.BoundedSemaphore()
+        self.textLock = asyncio.BoundedSemaphore()
+        self.linkLock = asyncio.BoundedSemaphore()
+        self.runIntervalLock = asyncio.BoundedSemaphore()
         self.started = False
 
     @tasks.loop(seconds=check_reminder_interval)
     async def check_reminder(self):
-        if (self.lastRunTime is None or self.lastRunTime + timedelta(seconds=self.runInterval) < datetime.now()) \
-                and self.notificationText is not None:
-            await self.send_reminder(self.notificationText)
+        async with self.runIntervalLock:
+            async with self.textLock:
+                if (self.lastRunTime is None or self.lastRunTime + timedelta(seconds=self.runInterval) < datetime.now()) \
+                        and self.notification_text is not None:
+                    await self.send_reminder(self.notification_text)
 
     @commands.command()
     async def link(self, _, arg):
-        self.notificationLink = arg
+        async with self.linkLock:
+            self.notification_link = arg
 
     @commands.command()
     async def text(self, _, arg):
-        self.notificationText = arg
+        async with self.textLock:
+            self.notification_text = arg
 
     @commands.command()
     async def interval(self, _, arg):
         try:
             parsed_int = int(arg)
             if parsed_int > check_reminder_interval:
-                self.runInterval = parsed_int
+                async with self.runIntervalLock:
+                    self.runInterval = parsed_int
             else:
                 await self.send("Value must be > {0}".format(check_reminder_interval))
         except ValueError:
@@ -62,8 +69,8 @@ class NotificationCog(commands.Cog):
             await self.send('Please provide two parameters.\r\n Ex: !set "Some kind of text" https://google.com')
             return
 
-        self.notificationText = args[0]
-        self.notificationLink = args[1]
+        self.notification_text = args[0]
+        self.notification_link = args[1]
 
     @check_reminder.before_loop
     async def before_check_reminder_loop(self):
@@ -76,7 +83,8 @@ class NotificationCog(commands.Cog):
     async def send_reminder(self, notification):
         self.lastRunTime = datetime.now()
         if notification is not None:
-            await self.send('{0}\n{1}'.format(notification,
-                                              self.notificationLink if
-                                              self.notificationLink is not None
-                                              else ""))
+            async with self.linkLock:
+                await self.send('{0}\n{1}'.format(notification,
+                                                  self.notification_link if
+                                                  self.notification_link is not None
+                                                  else ""))
